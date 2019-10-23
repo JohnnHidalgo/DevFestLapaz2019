4b6390f42390064d9d9d6959ffb8b119f3e9992a
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-10-23 17:02:34 -0400

    Add constants for AOG

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 'use strict';
-
-const { dialogflow } = require('actions-on-google');
-
+const {
+    dialogflow,
+    BasicCard,
+    Permission,
+    Suggestions,
+} = require('actions-on-google');
 const functions = require('firebase-functions');
-
 const app = dialogflow({ debug: true });
 
 const speakerMap = {
