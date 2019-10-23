42e4f086c472a7cbe79e2760c38eb7caf4fba73b
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-10-23 16:58:06 -0400

    Add Speakers Info

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,140 @@
-// Copyright 2018, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the 'License');
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an 'AS IS' BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 'use strict';
 
-// Import the Dialogflow module from the Actions on Google client library.
-const {dialogflow} = require('actions-on-google');
+const { dialogflow } = require('actions-on-google');
 
-// Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
 
-// Instantiate the Dialogflow client.
-const app = dialogflow({debug: true});
+const app = dialogflow({ debug: true });
 
-// Handle the Dialogflow intent named 'favorite color'.
-// The intent collects a parameter named 'color'.
-app.intent('favorite color', (conv, {color}) => {
+const speakerMap = {
+
+
+    'Pablo Perez': {
+        title: 'Pablo Perez',
+        image: {
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_PabloPerez.jpg?alt=media&token=4ac9f6ac-674b-4a06-898e-52177374b7b2',
+            accessibilityText: 'Pablo Perez',
+        },
+        display: 'WHITE',
+    },
+
+
+    'Argel Bejarano': {
+        title: 'Argel Bejarano',
+        image: {
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_ArgelBejarano.jpeg?alt=media&token=6e7f7398-9e4c-4797-be76-80bd9b601add',
+            accessibilityText: 'Argel Bejarano',
+        },
+        display: 'WHITE',
+    },
+
+
+    'Victor Aguilar': {
+        title: 'Victor Aguilar',
+        image: {
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_VictorAguilar.jpeg?alt=media&token=3c5aeec9-23b8-42de-9505-1fc32fd1cada',
+            accessibilityText: 'Victor Aguilar',
+        },
+        display: 'WHITE',
+    },
+
+
+    'Noe Bautista': {
+        title: 'Noe Bautista',
+        image: {
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_NoeBranagan.jpg?alt=media&token=63da5be6-45f1-43c8-9ac2-a7f0dba0a5f5',
+            accessibilityText: 'Noe Bautista',
+        },
+        display: 'WHITE',
+    },
+
+
+    'Adriana Moya': {
+        title: 'Adriana Moya',
+        image: {
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_AdrianaMoya.jpeg?alt=media&token=1b1e0b4d-69ac-4474-b247-1ab9b8bf789e',
+            accessibilityText: 'Adriana Moya',
+        },
+        display: 'WHITE',
+    },
+
+
+
+    'Leonidas Esteban': {
+        title: 'Leonidas Esteban',
+        image: {
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_LeonidasEstevan.jpg?alt=media&token=296f720c-c863-41d3-bf95-5ab1ef2a40aa',
+            accessibilityText: 'Leonidas Esteban',
+        },
+        display: 'WHITE',
+    },
+
+
+
+
+    'Diego Velasquez': {
+        title: 'Diego Velasquez',
+        image: {
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_DiegoVel%C3%A1squez.jpg?alt=media&token=fb6ad58c-957c-461e-bbe3-9bf3c1331478',
+            accessibilityText: 'Diego Velasquez',
+        },
+        display: 'WHITE',
+    },
+
+
+
+
+    'Fernanda Ramirez': {
+        title: 'Fernanda Ramirez',
+        image: {
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_FernandaRamirez.JPG?alt=media&token=be43db13-fff6-4b2a-8847-67b2bdbf944c',
+            accessibilityText: 'Fernanda Ramirez',
+        },
+        display: 'WHITE',
+    },
+
+
+
+    'Luis Avilés': {
+        title: 'Luis Avilés',
+        image: {
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_LuisAviles.png?alt=media&token=af1dc44e-eb16-401e-ae83-9d8a400b3ee6',
+            accessibilityText: 'Luis Avilés',
+        },
+        display: 'WHITE',
+    },
+
+
+    'Kenji Kawada': {
+        title: 'Kenji Kawada',
+        image: {
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_KenjiKawada.jpg?alt=media&token=28d0d294-4160-4e8b-9e8c-563842fce50c',
+            accessibilityText: 'Kenji Kawada',
+        },
+        display: 'WHITE',
+    },
+
+
+
+};
+
+var elem = [
+    ['Pablo Perez', 'Argentina'],
+    ['Argel Bejarano', 'Mexico'],
+    ['Victor Aguilar', 'Bolivia'],
+    ['Noe Bautista', 'Republica Dominicana'],
+    ['Adriana Moya', 'Colombia'],
+    ['Leonidas Esteban', 'Mexico'],
+    ['Diego Velasquez', 'Peru'],
+    ['Fernanda Ramirez', 'Mexico'],
+    ['Luis Avilés', 'Bolivia'],
+    ['Kenji Kawada', 'Bolivia'],
+];
+
+
+app.intent('favorite color', (conv, { color }) => {
     const luckyNumber = color.length;
-    // Respond with the user's lucky number and end the conversation.
     conv.close('Your lucky number is ' + luckyNumber);
 });
 
-// Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
