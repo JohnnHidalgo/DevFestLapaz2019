3c3b8d92b5c32d6fa9d0509df9b52b0c492663d9
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-10-27 16:19:23 -0400

    Update data of cardSpeakers

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,7 @@ const cardSpeakers = {
             url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_PabloPerez.jpg?alt=media&token=4ac9f6ac-674b-4a06-898e-52177374b7b2',
             accessibilityText: 'Pablo Perez',
         },
-        display: 'WHITE',
+        display: 'BLUE',
     },
     'Argel Bejarano': {
         title: 'Argel Bejarano',
@@ -24,7 +24,7 @@ const cardSpeakers = {
             url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_ArgelBejarano.jpeg?alt=media&token=6e7f7398-9e4c-4797-be76-80bd9b601add',
             accessibilityText: 'Argel Bejarano',
         },
-        display: 'WHITE',
+        display: 'BLUE',
     },
     'Victor Aguilar': {
         title: 'Victor Aguilar',
@@ -32,7 +32,7 @@ const cardSpeakers = {
             url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_VictorAguilar.jpeg?alt=media&token=3c5aeec9-23b8-42de-9505-1fc32fd1cada',
             accessibilityText: 'Victor Aguilar',
         },
-        display: 'WHITE',
+        display: 'BLUE',
     },
     'Noe Bautista': {
         title: 'Noe Bautista',
@@ -40,15 +40,15 @@ const cardSpeakers = {
             url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_NoeBranagan.jpg?alt=media&token=63da5be6-45f1-43c8-9ac2-a7f0dba0a5f5',
             accessibilityText: 'Noe Bautista',
         },
-        display: 'WHITE',
+        display: 'BLUE',
     },
     'Adriana Moya': {
         title: 'Adriana Moya',
         image: {
-            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_AdrianaMoya.jpeg?alt=media&token=1b1e0b4d-69ac-4474-b247-1ab9b8bf789e',
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_AdrianaMoya.jpeg?alt=media&token=fc248a76-f3eb-4448-9474-a5a1f3ed9374',
             accessibilityText: 'Adriana Moya',
         },
-        display: 'WHITE',
+        display: 'BLUE',
     },
     'Leonidas Esteban': {
         title: 'Leonidas Esteban',
@@ -56,15 +56,15 @@ const cardSpeakers = {
             url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_LeonidasEstevan.jpg?alt=media&token=296f720c-c863-41d3-bf95-5ab1ef2a40aa',
             accessibilityText: 'Leonidas Esteban',
         },
-        display: 'WHITE',
+        display: 'BLUE',
     },
     'Diego Velasquez': {
         title: 'Diego Velasquez',
         image: {
-            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_DiegoVel%C3%A1squez.jpg?alt=media&token=fb6ad58c-957c-461e-bbe3-9bf3c1331478',
+            url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_DiegoVel%C3%A1squez.jpg?alt=media&token=8ffa408b-1867-415f-bbf6-2fabf260b5c7',
             accessibilityText: 'Diego Velasquez',
         },
-        display: 'WHITE',
+        display: 'BLUE',
     },
     'Fernanda Ramirez': {
         title: 'Fernanda Ramirez',
@@ -72,7 +72,7 @@ const cardSpeakers = {
             url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_FernandaRamirez.JPG?alt=media&token=be43db13-fff6-4b2a-8847-67b2bdbf944c',
             accessibilityText: 'Fernanda Ramirez',
         },
-        display: 'WHITE',
+        display: 'BLUE',
     },
     'Luis Avilés': {
         title: 'Luis Avilés',
@@ -80,7 +80,7 @@ const cardSpeakers = {
             url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_LuisAviles.png?alt=media&token=af1dc44e-eb16-401e-ae83-9d8a400b3ee6',
             accessibilityText: 'Luis Avilés',
         },
-        display: 'WHITE',
+        display: 'BLUE',
     },
     'Kenji Kawada': {
         title: 'Kenji Kawada',
@@ -88,7 +88,7 @@ const cardSpeakers = {
             url: 'https://firebasestorage.googleapis.com/v0/b/devfestgdglapaz-mkrvwj.appspot.com/o/S_KenjiKawada.jpg?alt=media&token=28d0d294-4160-4e8b-9e8c-563842fce50c',
             accessibilityText: 'Kenji Kawada',
         },
-        display: 'WHITE',
+        display: 'BLUE',
     },
 };
 
